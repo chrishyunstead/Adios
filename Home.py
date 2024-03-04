3bc57d120467d7fb1cae761da17ce3e26789e125
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 00:54:43 +0900

    Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_pages import st_pages
 from st_pages import Page,show_pages,add_page_title,Section
 
 st.title('Adios Team Project\n부산항 입항 외국인 선원 대상 마케팅 인사이트 및 서비스 제공')
