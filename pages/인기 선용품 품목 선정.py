9763f292c6ea38da5bf33fd526203f8f63d39ace
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 20:46:12 +0900

    pages

--- pages/인기 선용품 품목 선정.py
+++ pages/인기 선용품 품목 선정.py
@@ -1,16 +1,7 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import glob
 import streamlit as st
 
-file_list_18=glob.glob('data/2018.*')
-df_list=[]
-for i in file_list_18:
-    df=pd.read_csv(i)
-    df['년도']=2018
-    df['분기']=i[10:13]
-    df_list.append(df)
-
-popular_ship_goods_df_18=pd.concat(df_list).sort_values('년도').reset_index().drop('index',axis=1)
-st.dataframe(popular_ship_goods_df_18,use_container_width=True)
+popular_ship_goods_18_df=pd.read_csv('data/18년도-선용품-품목.csv').drop('Unnamed: 0',axis=1)
+st.dataframe(popular_ship_goods_18_df,use_container_width=True)
