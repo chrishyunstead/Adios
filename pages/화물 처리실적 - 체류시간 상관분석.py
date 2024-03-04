422c2359df95cb89e8a70d592ebf6459320b9cb6
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 01:53:40 +0900

    pages

--- pages/화물 처리실적 - 체류시간 상관분석.py
+++ pages/화물 처리실적 - 체류시간 상관분석.py
@@ -6,3 +6,5 @@ import plotly.graph_objects as go
 from sklearn.preprocessing import MinMaxScaler
 import scipy.stats as stats
 
+ship_stayed_df=pd.read_csv('data/처리실적-체류시간_상관분석.csv').drop('Unnamed: 0',axis=1)
+st.dataframe(ship_stayed_df,use_container_width=True)
