49c61e06c31d823478a9c258789a681f2842770f
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 11:09:10 +0900

    update

--- pages/기업 리스트.py
+++ pages/기업 리스트.py
@@ -7,8 +7,7 @@ st.title('업종별 기업 리스트 소싱')
 company_list_df=pd.read_excel('data/기업엑셀화.xlsx').drop('Unnamed: 0',axis=1)
 company_type_list=list(set(company_list_df['업종']))
 company_type=st.multiselect('업종 선택',
-                            company_type_list,
-                            hide_index=True)
+                            company_type_list)
 company_selected_type=company_list_df.query(f'업종 in {company_type}').reset_index().drop('index',axis=1)
 st.dataframe(company_selected_type,
              use_container_width=True)
