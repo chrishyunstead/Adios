da39ae34927107779ef29be055491927e0a0e771
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 02:05:51 +0900

    pages

--- pages/화물 처리실적 - 체류시간 상관분석.py
+++ pages/화물 처리실적 - 체류시간 상관분석.py
@@ -6,14 +6,15 @@ import plotly.graph_objects as go
 from sklearn.preprocessing import MinMaxScaler
 import scipy.stats as stats
 
-st.title('화물처리실적-체류시간 상관분석')
+st.title('화물처리량-체류시간 상관분석')
 ship_stayed_df=pd.read_csv('data/처리실적-체류시간_상관분석.csv').drop('Unnamed: 0',axis=1)
-ship_stayed_df.rename(columns={'컨테이너':'물동량',
+ship_stayed_df.rename(columns={'컨테이너':'화물처리량',
                                '체류시간_totalTime(시간)':'체류시간'},
                       inplace=True)
 st.dataframe(ship_stayed_df,use_container_width=True)
 
 scaler=MinMaxScaler()
+ship_stayed_df.set_index('년도',inplace=True)
 ship_stayed_scaled=pd.DataFrame(
     scaler.fit_transform(ship_stayed_df.copy()),
     columns=ship_stayed_df.columns
@@ -24,13 +25,13 @@ ship_stayed_scaled.reset_index(inplace=True)
 def ts_plotly_gen():
     fig=go.Figure()
     fig.add_trace(go.Scatter(x=ship_stayed_scaled['년도'],
-                             y=ship_stayed_scaled['물동량'],
+                             y=ship_stayed_scaled['화물처리량'],
                              name='물동량'))
     fig.add_trace(go.Scatter(x=ship_stayed_scaled['년도'],
                              y=ship_stayed_scaled['체류시간'],
                              name='체류시간'))
     fig.update_layout(
-        title='연간 물동량, 체류시간 추이',
+        title='연간 화물처리량, 체류시간 추이',
         xaxis=dict(title='년도'),
         annotations=[
             dict(x=-0.03,
