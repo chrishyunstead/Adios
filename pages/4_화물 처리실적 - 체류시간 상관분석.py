1fe2698dab4ab7dc713c13188f3d6833cfb2df64
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 03:07:48 +0900

    pages

--- pages/4_화물 처리실적 - 체류시간 상관분석.py
+++ pages/4_화물 처리실적 - 체류시간 상관분석.py
@@ -38,9 +38,6 @@ def plotly_gen_corr():
     fig=go.Figure()
     fig.add_trace(go.Scatter(x=ship_stayed_scaled['체류시간'],
                              y=ship_stayed_scaled['화물처리량'],
-                             hovertemplate='<b>체류시간:</b> %{text1}\n<b>화물처리량:</b> %{text2}',
-                             text1=ship_stayed_scaled['체류시간'],
-                             text2=ship_stayed_scaled['화물처리량'],
                              mode='markers',name='실측값'))
 
     # 추세선을 추가합니다.
