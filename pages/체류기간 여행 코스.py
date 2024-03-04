9b926774149a060de49b9dd57351dc08d51a217f
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 15:19:25 +0900

    pages

--- pages/체류기간 여행 코스.py
+++ pages/체류기간 여행 코스.py
@@ -28,8 +28,7 @@ busanport_coord=[35.1029191, 129.0407161]
 
 def base_osmnx_gen():
     G=ox.graph_from_point(busanport_coord,network_type='drive',dist=5000)
-    fig,ax=ox.plot_graph(G,node_color='black',node_size=5,bgcolor='w')
-    return fig
+    return G
 
 def osmnx_gen():
     G1=base_osmnx_gen()
