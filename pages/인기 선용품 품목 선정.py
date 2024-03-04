0bf99b6d1aa2399904d055790b947d8190c32ba9
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 23:23:51 +0900

    pages

--- pages/인기 선용품 품목 선정.py
+++ pages/인기 선용품 품목 선정.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
-popular_ship_goods_18_df=pd.read_csv('data/18년도-선용품-품목.csv').drop('Unnamed: 0',axis=1)
+popular_ship_goods_18_df=pd.read_csv('data/2018년도 분기별 품목 합.csv').drop('Unnamed: 0',axis=1)
 st.dataframe(popular_ship_goods_18_df,use_container_width=True)
