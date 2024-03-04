d8dcf9cfcf54c82c1577575b7e5b8f8cae9169f6
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 10:31:15 +0900

    pages

---
+++ pages/기업 리스트.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('업종별 기업 리스트 소싱')
+
+company_list_df=pd.read_excel('data/기업엑셀화.xlsx').dorp('Unnamed: 0',axis=1)
+company_type_list=list(set(company_list_df['업종']))
+company_type=st.multiselect('업종 선택',company_type_list)
+company_selected_type=company_list_df.query(f'업종 in {company_type}')
+st.dataframe(company_selected_type)
