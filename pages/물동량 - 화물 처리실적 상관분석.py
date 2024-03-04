e07faf43b2185bbfb7bfa358225ab3e759ef0fb7
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 01:47:20 +0900

    pages

--- pages/물동량 - 화물 처리실적 상관분석.py
+++ pages/물동량 - 화물 처리실적 상관분석.py
@@ -22,8 +22,34 @@ corr=stats.pearsonr(ton_shipgoods_scaled['물동량'],
 st.dataframe(ton_shipgoods_df,use_container_width=True)
 st.write(f"상관계수 : {round(corr[0],3)}")
 st.write(f"P-values : {round(corr[1],3)}")
+def plotly_gen_corr():
+    fig=go.Figure()
+    fig.add_trace(go.Scatter(
+        x=ton_shipgoods_scaled['물동량'],
+        y=ton_shipgoods_scaled['화물처리실적'],
+        mode='markers',name='실측값'
+    ))
+    fig.add_trace(go.Scatter(
+        x=ton_shipgoods_scaled['물동량'],
+        y=np.poly1d(np.polyfit(ton_shipgoods_scaled['물동량'],ton_shipgoods_scaled['화물처리실적'],1))\
+            (ton_shipgoods_scaled['물동량']),
+        mode='lines',name='추세선'
+    ))
+    fig.update_layout(title='연간 물동량 - 화물처리실적 상관분석',
+                    xaxis=dict(title='물동량'),
+                    yaxis=dict(title='화물처리실적'),
+                    annotations=[
+                        dict(x=-0.03,
+                            y=1.15,
+                            xref='paper',
+                            yref='paper',
+                            text=f'상관계수 : {round(corr[0],3)}\t(정규화된 수치로 계산)',
+                            showarrow=False)])
+    return fig
+st.plotly_chart(plotly_gen_corr())
 
-def plotly_gen():
+
+def plotly_gen_1():
     fig=go.Figure()
     fig.add_trace(go.Scatter(x=ton_shipgoods_scaled['년도'],
                              y=ton_shipgoods_scaled['물동량'],
@@ -44,4 +70,4 @@ def plotly_gen():
                             showarrow=False)])
     return fig
 
-st.plotly_chart(plotly_gen())
+st.plotly_chart(plotly_gen_1())
---
+++ pages/화물 처리실적 - 체류시간 상관분석.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly
+import plotly.graph_objects as go
+from sklearn.preprocessing import MinMaxScaler
+import scipy.stats as stats
+
