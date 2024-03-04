7245bf18bf1227e1583fa9a2c69bf74daa4ddc9f
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 18:29:39 +0900

    pages

--- pages/체류기간 여행 코스.py
+++ pages/체류기간 여행 코스.py
@@ -16,6 +16,8 @@ restaurant_df=restaurant_df_raw.query(f"구분=='{restaurant_type_selet}'")\
     .reset_index().drop('index',axis=1)
 st.dataframe(restaurant_df,use_container_width=True)
 restaurant_select=st.selectbox('식당 선택',restaurant_df['가게명'])
+restaurant_selected_df=restaurant_df.query(f"가게명=='{restaurant_select}'")\
+    .reset_index().drop('index',axis=1)
 
 hotel_type_list=list(set(hotel_df_raw['구분']))
 hotel_grade_select=st.selectbox('호텔 등급 선택',hotel_type_list)
@@ -23,26 +25,16 @@ hotel_df=hotel_df_raw.query(f"구분=='{hotel_grade_select}'")\
     .reset_index().drop('index',axis=1)
 st.dataframe(hotel_df,use_container_width=True)
 hotel_select=st.selectbox('호텔 선택',hotel_df['업소명'])
+hotel_selected_df=hotel_df.query(f"업소명=='{hotel_select}'")\
+    .reset_index().drop('index',axis=1)
 
 
 # 부산 신항 좌표
 busanport_coord=[35.078205, 128.832975]
-def rest_coord_gen():
-    restaurant_selected_df=restaurant_df.query(f"가게명=='{restaurant_select}'")\
-        .reset_index().drop('index',axis=1)
 
-    restaurant_coord=[restaurant_selected_df['lat'][0],
-                        restaurant_selected_df['lng'][0]]
-    return(restaurant_coord)
-
-def hotel_coord_gen():
-    hotel_selected_df=hotel_df.query(f"업소명=='{hotel_select}'")\
-        .reset_index().drop('index',axis=1)
-    hotel_coord=[hotel_selected_df['lat'][0],hotel_selected_df['lng'][0]]
-    return hotel_coord
-
-restaurant_coord=rest_coord_gen()
-hotel_coord=hotel_coord_gen()
+restaurant_coord=[restaurant_selected_df['lat'][0],
+                  restaurant_selected_df['lng'][0]]
+hotel_coord=[hotel_selected_df['lat'][0],hotel_selected_df['lng'][0]]
 
 def base_osmnx_gen():
     G=ox.graph_from_point(busanport_coord,network_type='all',dist=7000)
