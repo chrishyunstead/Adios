9b558a1a0ff14b40b5aad296ec50c0ed3e58359d
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 14:48:59 +0900

    pages

--- pages/체류기간 여행 코스.py
+++ pages/체류기간 여행 코스.py
@@ -8,29 +8,42 @@ import networkx as nx
 restaurant_df_raw=pd.read_excel('data/평점 4이상 맛집리스트.xlsx').drop('Unnamed: 0',axis=1)
 hotel_df_raw=pd.read_excel('data/평점 4이상 숙박업소.xlsx').drop('Unnamed: 0',axis=1)
 
-
-busanport_coord=[35.1029191, 129.0407161]
-@st.cache_data
-def base_osmnx_gen():
-    G=ox.graph_from_point(busanport_coord,network_type='all',dist=5000)
-    return G
-
 restaurant_type_list=list(set(restaurant_df_raw['구분']))
 restaurant_type_selet=st.selectbox('식당 분류 선택',restaurant_type_list)
 restaurant_df=restaurant_df_raw.query(f"구분=='{restaurant_type_selet}'")\
     .reset_index().drop('index',axis=1)
 st.dataframe(restaurant_df)
-restaurant_df_selected=st.selectbox('식당 선택',restaurant_df['가게명'])
+restaurant_select=st.selectbox('식당 선택',restaurant_df['가게명'])
+restaurant_selected_df=restaurant_df.query(f"가게명=={restaurant_select}")
 
 hotel_type_list=list(set(hotel_df_raw['구분']))
-hotel_type_select=st.selectbox('호텔 등급 선택',hotel_type_list)
-hotel_df=hotel_df_raw.query(f"구분=='{hotel_type_select}'")\
+hotel_grade_select=st.selectbox('호텔 등급 선택',hotel_type_list)
+hotel_df=hotel_df_raw.query(f"구분=='{hotel_grade_select}'")\
     .reset_index().drop('index',axis=1)
 st.dataframe(hotel_df)
-hotel_df_selected=st.selectbox('호텔 선택',hotel_df['업소명'])
+hotel_select=st.selectbox('호텔 선택',hotel_df['업소명'])
+hotel_selected_df=hotel_df.query(f"업소명=={hotel_select}")
 
-G1=base_osmnx_gen()
-busan_port=ox.didstance.nearest_nodes(G1,
-                                      busanport_coord[1],
-                                      busanport_coord[0])
+busanport_coord=[35.1029191, 129.0407161]
+restaurant_coord=[restaurant_selected_df['lat'],restaurant_selected_df['lng']]
+hotel_coord=[hotel_selected_df['lat'],hotel_selected_df['lng']]
+@st.cache_data
+def base_osmnx_gen():
+    G=ox.graph_from_point(busanport_coord,network_type='all',dist=2000)
+    return G
 
+def osmnx_gen():
+    G1=base_osmnx_gen()
+    busan_port_point=ox.distance.nearest_nodes(G1,busanport_coord[1],busanport_coord[0])
+    restaurant_point=ox.distance.nearest_nodes(G1,restaurant_coord[1],restaurant_coord[0])
+    hotel_point=ox.distance.nearest_nodes(G1,hotel_coord[1],hotel_coord[0])
+
+    route1=nx.shortest_path(G1,busan_port_point,restaurant_point)
+    route2=nx.shortest_path(G1,restaurant_coord,hotel_point)
+
+    fig,ax=ox.plot_routes(G1,[route1,route2],node_size=0.5,
+                          edge_linewidth=0.5,edge_color='w',
+                          route_colors=['blue','red'])
+    return(fig)
+
+osmnx_gen()
