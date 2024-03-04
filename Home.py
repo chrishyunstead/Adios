91176f866e8ab858e61924a410a29922d1e20f88
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 01:40:05 +0900

    Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from st_pages import Page,show_pages,add_page_title,Section
 
 st.title('Adios Team Project\n부산항 입항 외국인 선원 대상 마케팅 인사이트 및 서비스 제공')
     
@@ -9,5 +8,7 @@ st.markdown(
     -----
     * 페이지 업데이트
         - 인기 선용품 기업 리스트 소싱
+        - 인기 선용품 품목 분석
+        - 물동량 - 화물처리실적 상관분석
 '''
 )
