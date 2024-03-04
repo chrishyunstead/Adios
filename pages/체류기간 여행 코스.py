0ad8c7c78ae5d9d83facabc6641c0410324056eb
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-04 16:46:22 +0900

    pages

--- pages/체류기간 여행 코스.py
+++ pages/체류기간 여행 코스.py
@@ -52,11 +52,11 @@ def osmnx_gen():
     route1=nx.shortest_path(target_point,busan_port_point,restaurant_point)
     route2=nx.shortest_path(target_point,restaurant_point,hotel_point)
 
-    route_map=ox.plot_route_folium(target_point,route1,popup_attribute='length',
+    route_map=ox.plot_route_folium(target_point,route1,popup_attribute='time',
                                    color='blue')
     folium=ox.plot_route_folium(target_point,route2,
                                 route_map=route_map,
-                                popup_attribute='length',
+                                popup_attribute='time',
                                 color='red')
     return(folium)
 
