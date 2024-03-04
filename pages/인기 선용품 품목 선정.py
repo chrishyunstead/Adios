370b62b0f7f622b6556ec327f00a850c6634174d
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 00:24:13 +0900

    pages

--- pages/인기 선용품 품목 선정.py
+++ pages/인기 선용품 품목 선정.py
@@ -10,9 +10,9 @@ from matplotlib import font_manager, rc
 plt.rcParams['axes.unicode_minus']=False
 f_path='Font/경기천년바탕OTF_BOLD.OTF'
 font_name=font_manager.FontProperties(fname=f_path).get_name()
-st.write(f'{font_name}')
 rc('font',family=font_name)
 
+st.title('년도 / 분기별 선용품 품목 시각화')
 popular_ship_goods_18_df=pd.read_csv('data/2018년도 분기별 품목 합.csv')
 popular_ship_goods_18_df['년도']=2018
 popular_ship_goods_19_df=pd.read_csv('data/2019년도 분기별 품목 합.csv')
@@ -26,24 +26,39 @@ popular_ship_goods_22_df['년도']=2022
 popular_ship_goods_23_df=pd.read_csv('data/2023년도 분기별 품목 합.csv')
 popular_ship_goods_23_df['년도']=2023
 
-st.dataframe(popular_ship_goods_18_df,use_container_width=True)
-title_list=['2018년도 1분기 선용품 품목',
-            '2018년도 2분기 선용품 품목',
-            '2018년도 3분기 선용품 품목',
-            '2018년도 4분기 선용품 품목']
+df_list=[popular_ship_goods_18_df,
+         popular_ship_goods_19_df,
+         popular_ship_goods_20_df,
+         popular_ship_goods_21_df,
+         popular_ship_goods_22_df,
+         popular_ship_goods_23_df]
+for i in df_list:
+    i.columns=['품목','1분기','2분기','3분기','4분기','년도']
+df_concated=pd.concat(df_list)
+
+select_year=st.selectbox('년도 선택',list(set(df_concated.year)))
+df_selected=df_concated.query(f"년도=={select_year}")
+
+st.dataframe(df_selected,use_container_width=True)
+
+
+title_list=[f'{select_year}년도 1분기 선용품 품목',
+            f'{select_year}년도 2분기 선용품 품목',
+            f'{select_year}년도 3분기 선용품 품목',
+            f'{select_year}년도 4분기 선용품 품목']
 def plotly_gen():
     fig=make_subplots(rows=2,cols=2,column_titles=title_list)
-    fig.add_trace(go.Bar(x=popular_ship_goods_18_df['품목'],
-                             y=popular_ship_goods_18_df['2018.1분기'],
+    fig.add_trace(go.Bar(x=df_selected['품목'],
+                             y=df_selected[f'{select_year}.1분기'],
                              marker_color='#ffff00'),row=1,col=1)
-    fig.add_trace(go.Bar(x=popular_ship_goods_18_df['품목'],
-                             y=popular_ship_goods_18_df['2018.2분기'],
+    fig.add_trace(go.Bar(x=df_selected['품목'],
+                             y=df_selected[f'{select_year}.2분기'],
                              marker_color='#40e0d0'),row=1,col=2)
-    fig.add_trace(go.Bar(x=popular_ship_goods_18_df['품목'],
-                             y=popular_ship_goods_18_df['2018.3분기'],
+    fig.add_trace(go.Bar(x=df_selected['품목'],
+                             y=df_selected[f'{select_year}.3분기'],
                              marker_color='#ff6600'),row=2,col=1)
-    fig.add_trace(go.Bar(x=popular_ship_goods_18_df['품목'],
-                             y=popular_ship_goods_18_df['2018.4분기'],
+    fig.add_trace(go.Bar(x=df_selected['품목'],
+                             y=df_selected[f'{select_year}.4분기'],
                              marker_color='#7749f5'),row=2,col=2)
     fig.update_layout(width=1300,height=900)
     return fig
