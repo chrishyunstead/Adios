231f314075929dec561dad25dd8ea331fc5df159
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 13:46:31 +0900

    src

--- pages/라-1) 맛집 시각화.py
+++ pages/라-1) 맛집 시각화.py
@@ -5,4 +5,6 @@ import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import st_folium
 
+st.set_page_config(layout='wide')
+st.title('맛집 시각화')
 df=pd.read_excel('data/평점 4이상 맛집리스트.xlsx',index_col=0)
