d39cbb5e1a5e01458107c08d95584604faba8c82
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 12:59:55 +0900

    ds

--- Abstract.py
+++ Abstract.py
@@ -1,10 +1,15 @@
 import streamlit as st
 st.set_page_config(layout='wide')
-st.markdown('''
+
+html = """
+    <div class="flourish-embed flourish-bar-chart-race" data-src="visualisation/17012869"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+"""
+
+st.markdown(f'''
       # 1. 내용
          ## 가) 대한민국 수출입 물동량
          1) 23년 2분기 대한민국 수출입 물동량은 전년 동기대비(3억 1,849만 톤) 0.5% 감소한 3억 1,695만 톤으로 집계되었으며 연안 물동량은 전년 동기(6,084만 톤) 대비 7.6% 감소한 5,622만 톤으로 집계 되었고, 부산항은 전년 동기대비 –0.1% 감소했지만 매년 물동량이 증가되는 추세임.
-         2) Floulish 배치
+         2) {st.markdown(html, unsafe_allow_html=True)}
          3) 년도별 부산항 물동량, 시계열분석
          4) 같이 매년 부산항 물동량은 지속해서 증가되고 있으며, 물동량 데이터 기반의 시계열 예측 결과인 그림 ② 처럼 물동량 추세 또한 증가되고 있음을 참고할 수 있음. 따라서 물동량이 증가되는 만큼 부산항에 입/출항하는 선박의 수와 부산에 체류하는 외국인 선원 수 또한 증가할 것으로 예상할 수 있음.
          ## 나) 부산항만공사에 제안 서비스관련 데이터 분석
@@ -13,3 +18,5 @@ st.markdown('''
          3) 위 상황을 고려하여 외국인 선원을 대상으로 ‘온라인 선용품 구매 플랫폼’과 ‘체류시간별 부산관광코스 정보제공 서비스’를 기획 하였고, 서비스별 필요한 과업에 따라 데이터 분석을 진행했음.
          4) 이미지
 ''')
+
+
