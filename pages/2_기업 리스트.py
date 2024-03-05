86477bdff7fed46d0de1f7bd9cafe4877450b456
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-05 10:16:32 +0900

    pages

--- pages/2_기업 리스트.py
+++ pages/2_기업 리스트.py
@@ -4,21 +4,11 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 st.title('업종별 기업 리스트 소싱')
-company_list_df=pd.read_excel('data/기업 엑셀화 최종본.xlsx').drop('Unnamed: 0',axis=1)
-company_list_df['대분류']=np.nan
-for i in list(company_list_df.index):
-    if company_list_df['업종'][i] in ['빵류, 과자류, 당류, 초콜릿 도매업','낙농품 및 동ㆍ식물성 유지 도매업','수산물 가공식품 도매업','기타 가공식품 도매업']:
-        company_list_df['대분류'][i]='식품'
-    elif company_list_df['업종'][i] in ['육류 가공식품 도매업','육류 도매업']:
-        company_list_df['대분류'][i]='육류'
-    elif company_list_df['업종'][i]=='주류 도매업':
-        company_list_df['대분류'][i]='주류'
-    else:
-        company_list_df['대분류'][i]='기념품'
-company_type_list=list(set(company_list_df['대분류']))
+company_list_df=pd.read_csv('data/카테고리별 기업 리스트 최종본.csv').drop('Unnamed: 0',axis=1)
+company_type_list=list(set(company_list_df['카테고리']))
 company_type=st.multiselect('업종 선택',
                             company_type_list)
-company_selected_type=company_list_df.query(f'대분류 in {company_type}').reset_index().drop('index',axis=1)
+company_selected_type=company_list_df.query(f'카테고리 in {company_type}').reset_index().drop('index',axis=1)
 st.dataframe(company_selected_type,
              use_container_width=True,
              height=600)
