9ccfd54dd8465441a9f346161e3f5ab9bd2bc8f7
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-07 20:00:40 +0900

    pages

--- entry.py
+++ entry.py
@@ -3,6 +3,6 @@ from st_pages import Page,Section,show_pages
 st.title('ㅎㅇ')
 show_pages=(
     [
-        Page('sub/가-1) 물동량 - 화물 처리실적 상관분석,py','물동량 - 화물처리실적',':graph:')
+        Page('sub/가-1) 물동량 - 화물 처리실적 상관분석,py','물동량 - 화물처리실적',':pig:')
     ]
 )
