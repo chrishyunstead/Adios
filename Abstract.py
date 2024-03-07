0f5b697168625c5d35067e7962a78b3d6a297850
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-07 19:44:27 +0900

    pages

--- Abstract.py
+++ Abstract.py
@@ -15,8 +15,8 @@ st.markdown('''
 ''')
 show_pages(
     [
-        Page('Abstract.py','Home',':home:'),
         Section(name='가) 개요'),
-        Page('sub_pages/가-1) 물동량 - 화물 처리실적 상관분석.py','물동량 - 화물처리실적',':chicken:')
+        Page('sub_pages/가-1) 물동량 - 화물 처리실적 상관분석.py','물동량 - 화물처리실적',':chicken:'),
+        Page('sub_pages/가-2) 화물 처리실적 - 체류시간 상관분석.py','화물처리실적 - 체류시간',':pig:')
     ]
 )
