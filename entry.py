cf4067c8d371366bfe7b1ef3c8b926567146ba58
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-07 19:53:10 +0900

    pages

---
+++ entry.py
@@ -0,0 +1,7 @@
+from st_pages import Page,Section,show_page
+show_page=(
+    [
+        Page('Abstract.py','가) 개요',':book:'),
+        Page('sub/가-1) 물동량 - 화물 처리실적 상관분석,py','물동량 - 화물처리실적',':graph:')
+    ]
+)
