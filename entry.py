f5d4a2bfeda1cf2cf267616283fa808bc43f8664
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-07 20:05:35 +0900

    pages

--- entry.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from st_pages import Page,Section,show_pages
-st.title('ㅎㅇ')
-show_pages=(
-    [
-        Page('sub/가-1) 물동량 - 화물 처리실적 상관분석,py','물동량 - 화물처리실적',':pig:')
-    ]
-)
