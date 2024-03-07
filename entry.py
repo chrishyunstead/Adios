cc520f0642f722bcb1277930eb3bc769714eff09
Kshi0219 <149676714+Kshi0219@users.noreply.github.com>
2024-03-07 20:00:08 +0900

    pages

--- entry.py
+++ entry.py
@@ -3,7 +3,6 @@ from st_pages import Page,Section,show_pages
 st.title('ㅎㅇ')
 show_pages=(
     [
-        Page('main/Abstract.py','가) 개요',':book:'),
         Page('sub/가-1) 물동량 - 화물 처리실적 상관분석,py','물동량 - 화물처리실적',':graph:')
     ]
 )
